5ea1b1f59e0a48687e83554ab55e825446ae1c7b
Kynan Song <kynan.song@bemo.co>
2018-04-18 16:56:52 +0100

    Added Stop image, and basic on/off logic for buttons.

--- UdacityPitchApp/ViewController.swift
+++ UdacityPitchApp/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("I was called")
+        stopRecordingButton.isEnabled = false
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        <#code#>
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -27,11 +39,15 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("Was Pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         print("Was Pressed, Stop")
-        recordingLabel.text = "Recording Stopped"
+        recordingLabel.text = "Press to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
     }
 }
 
